65087593a6b2d2504565993026ef73df2c17f016
ms0004284 <ms0004284@gmail.con>
2018-06-21 02:08:19 +0800

    增加鬧鐘

--- app/src/main/java/com/example/a36432/superalarmclock/WakeupActivity.java
+++ app/src/main/java/com/example/a36432/superalarmclock/WakeupActivity.java
@@ -12,7 +12,7 @@ public class WakeupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wakeup);
-
+        int i ;
         MediaPlayer mediaPlayer;
 
 
