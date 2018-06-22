e70636630db08f410b5bef958ab7f0bb755a5bb2
ms0004284 <d0589769@fcu.edu.tw>
2018-06-23 01:51:03 +0800

    增加鬧鐘

--- app/src/main/java/com/example/a36432/superalarmclock/MainActivity.java
+++ app/src/main/java/com/example/a36432/superalarmclock/MainActivity.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -19,13 +18,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AnalogClock;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
-
-import static android.graphics.Color.parseColor;
 
 public class MainActivity extends AppCompatActivity {
     private Button note;
@@ -45,7 +41,49 @@ public class MainActivity extends AppCompatActivity {
         note = (Button) findViewById(R.id.btn_memo);
         set = (Button) findViewById(R.id.btn_set);
         weather = (Button) findViewById(R.id.btn_weather);
+        clock = (AnalogClock) findViewById(R.id.Clock);
 
+        clock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                final int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+                // Create a new instance of TimePickerDialog and return it
+
+                new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minutes) {
+                        /*SQLiteDatabase db = helper.getReadableDatabase();
+                        ContentValues cv = new ContentValues();
+                        cv.put("hour", hourOfDay);
+                        cv.put("minute", minutes);
+                        db.insert(DATABASETABLE, null, cv);*/
+                        registerReceiver(new PlayReceiver() , new IntentFilter("clock"));
+
+                        Calendar mCal = Calendar.getInstance();
+
+                        mCal.setTimeInMillis(System.currentTimeMillis());
+                        mCal.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        mCal.set(Calendar.MINUTE,minutes);
+                        mCal.set(Calendar.SECOND,00);
+                        Intent intentAlarm = new Intent("clock");
+
+                        AlarmManager am = (AlarmManager) MainActivity.this.getSystemService(Context.ALARM_SERVICE);
+
+                      PendingIntent pi = PendingIntent.getBroadcast(MainActivity.this, 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+
+                        am.setRepeating(AlarmManager.RTC_WAKEUP, mCal.getTimeInMillis(),AlarmManager.INTERVAL_DAY, pi);
+                        Toast.makeText(MainActivity.this, "設置鬧鐘的時間為："+String.valueOf(hourOfDay)+":"+String.valueOf(minutes), Toast.LENGTH_SHORT).show();
+
+                    }
+                }, hour, minute, true).show();
+
+
+            }
+
+        });
 
         note.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,29 +118,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        final LinearLayout background = (LinearLayout)findViewById(R.id.back);
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_OK) {
-            return;
-        }
-        if (data==null){
-            return;
-        }
-        Bundle bundle =this.getIntent().getExtras();
-
-        int id = bundle.getInt("ID");
-        switch(id){
-            case R.id.btn_red:
-                background.setBackgroundColor(parseColor("#e3170d"));
-               break;
-        }
-
-    }
-
-
 
     class ClockDB extends SQLiteOpenHelper {
         public ClockDB(Context context) {
